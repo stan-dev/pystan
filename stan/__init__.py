f2649eae58fdb2290125961f298ea422f4ac5d66
Allen Riddell <riddella+stan@indiana.edu>
2018-10-16 16:12:45 -0400

    pbr needs package name not module name
    Package name remains `pystan`.

--- stan/__init__.py
+++ stan/__init__.py
@@ -1,6 +1,4 @@
 import pbr.version
-
 from stan.model import build  # noqa
 
-
-__version__ = pbr.version.VersionInfo("stan").version_string()
+__version__ = pbr.version.VersionInfo("pystan").version_string()
