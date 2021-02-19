45ef59b4555ac79f5bf9d79a673cbe5130aaaf42
Allen Riddell <riddella+stan@indiana.edu>
2021-02-19 07:06:31 -0500

    feat: Add plugin system
    Other developers can change the behavior of PyStan by writing a plugin.
    Plugin developers should create a class which subclasses
    `stan.plugins.PluginBase`. This class must be referenced in their
    package's entry points section.
    
    This feature is added with a particular use in mind. A developer now has
    the ability run HMC diagnostics when sampling has completed and alert
    the user if any problems are detected.
    
    Closes #129

---
+++ stan/plugins.py
@@ -0,0 +1,44 @@
+import abc
+from typing import Generator
+
+import pkg_resources
+
+import stan.fit
+
+
+def get_plugins() -> Generator[pkg_resources.EntryPoint, None, None]:
+    """Iterate over available plugins."""
+    return pkg_resources.iter_entry_points(group="stan.plugins")
+
+
+class PluginBase(abc.ABC):
+    """Base class for PyStan plugins.
+
+    Plugin developers should create a class which subclasses `PluginBase`.
+    This class must be referenced in their package's entry points section.
+
+    """
+
+    # Implementation note: this plugin system is simple because there are only
+    # a couple of places a plugin developer might want to change behavior. For
+    # a more full-featured plugin system, see Stevedore
+    # (<https://docs.openstack.org/stevedore>).  This plugin system follows
+    # (approximately) the pattern stevedore labels `ExtensionManager`.
+
+    def on_post_fit(self, fit: stan.fit.Fit) -> stan.fit.Fit:
+        """Called with Fit instance when sampling has finished.
+
+        The plugin can report information about the samples
+        contained in the Fit object. It may also add to or
+        modify the Fit instance.
+
+        If the plugin only analyzes the contents of `fit`,
+        it must return the `fit`.
+
+        Argument:
+            fit: Fit instance.
+
+        Returns:
+            The Fit instance.
+        """
+        return fit
