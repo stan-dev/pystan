b2d48bb84529ff537590c98ba9e676e13735aaa6
A. Riddell <riddella+stan@indiana.edu>
2022-02-15 15:23:46 -0500

    test: Update test for Stan 2.29

--- tests/test_stanc_warnings.py
+++ tests/test_stanc_warnings.py
@@ -14,7 +14,24 @@ def test_stanc_no_warning() -> None:
     assert "warning" not in buffer.getvalue().lower()
 
 
-def test_stanc_warning() -> None:
+def test_stanc_unused_warning() -> None:
+    """Test that stanc warning is shown to user."""
+    program_code = """
+    parameters {
+    real y;
+    }
+    model {
+    real x;
+    x = 5;
+    }
+    """
+    buffer = io.StringIO()
+    with contextlib.redirect_stderr(buffer):
+        stan.build(program_code=program_code)
+    assert "The parameter y was declared but was not used in the density" in buffer.getvalue()
+
+
+def test_stanc_assignment_warning() -> None:
     """Test that stanc warning is shown to user."""
     # stanc prints warning:
     # assignment operator <- is deprecated in the Stan language; use = instead.
@@ -25,9 +42,10 @@ def test_stanc_warning() -> None:
     model {
     real x;
     x <- 5;
+    y ~ normal(0,1);
     }
     """
     buffer = io.StringIO()
     with contextlib.redirect_stderr(buffer):
         stan.build(program_code=program_code)
-    assert "assignment operator <- is deprecated in the Stan language" in buffer.getvalue()
+    assert "operator <- is deprecated in the Stan language and will be removed" in buffer.getvalue(), buffer.getvalue()
